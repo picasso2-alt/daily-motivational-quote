b9f2301f60bdcbb1cedd808b896905f31fce3e23
picasso2-alt <nicolas.bosshardt@hey.com>
2024-12-18 22:27:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from email.mime.multipart import MIMEMultipart
 
 # Function to fetch a motivational quote
 def get_motivational_quote():
-    response = requests.get("https://api.quotable.io/random")
+    response = requests.get("https://zenquotes.io/api/random")
     if response.status_code == 200:
-        quote_data = response.json()
-        return f'"{quote_data["content"]}" - {quote_data["author"]}'
+        quote_data = response.json()[0]
+        return f'"{quote_data["q"]}" - {quote_data["a"]}'
     else:
         return "Could not retrieve a quote."
 
